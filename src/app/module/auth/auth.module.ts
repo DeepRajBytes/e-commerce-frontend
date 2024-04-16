6b513a44de190529697d269c6b74ec4cb775c756
deeprajiflair <deepraj.chouhan@iflair.com>
2024-04-16 11:38:23 +0530

    prgress patch

--- src/app/module/auth/auth.module.ts
+++ src/app/module/auth/auth.module.ts
@@ -11,12 +11,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from "../shared/shared.module";
 
 
+
 @NgModule({
     declarations: [
         SigninComponent,
         SignupComponent,
-        AuthComponent
-    ],
+        AuthComponent,
+     ],
     imports: [
         CommonModule,
         MatButtonModule,
