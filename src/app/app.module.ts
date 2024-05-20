ebb8a0db56676fd8adb8e5790de8a0711c8204c6
deeprajiflair <deepraj.chouhan@iflair.com>
2024-05-20 12:06:53 +0530

    patch update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { SharedModule } from './module/shared/shared.module'
 import { AdminModule } from './module/admin/admin.module';
 import { StoreModule } from '@ngrx/store';
